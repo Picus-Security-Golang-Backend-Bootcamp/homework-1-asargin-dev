a728db8b0b7aeadfca8821afe8172929b318236c
asargin <asargin@tarla.io>
2022-02-27 15:26:01 +0300

    Package Usage Update

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/Picus-Security-Golang-Backend-Bootcamp/homework-1-asargin-dev/utils"
 )
 
 func main() {
@@ -24,12 +26,12 @@ func main() {
 
 		case "list":
 
-			list_book(books)
+			utils.List_book(books)
 
 		case "search":
 			// search argümanı sonrası girilececek değerleri Join ederek string bir arama querysi oluşturarak
 			// kitaplarla beraber search_book fonksiyonumuza parametre olarak gönderiyoruz.
-			search_book(strings.Join(args[2:], " "), books)
+			utils.Search_book(strings.Join(args[2:], " "), books)
 
 		}
 
@@ -39,48 +41,4 @@ func main() {
 
 	}
 
-}
-
-func list_book(books []string) {
-
-	if len(books) == 0 {
-
-		fmt.Println("Kitaplıkta kitap bulunmamaktadır.")
-
-	} else {
-
-		for _, book := range books {
-
-			fmt.Println(book)
-
-		}
-
-	}
-
-}
-
-func search_book(search_params string, books []string) {
-
-	if len(search_params) == 0 {
-
-		fmt.Println("Lütfen aramak istediğiniz kitabın adını yazınız.")
-
-	} else {
-		// Sonuçlar liste şeklinde istenildiğinden, İşlemler sonrası eşleşen sonuçları 'result' slice'ının içine append edeceğiz.
-		result := []string{}
-
-		for _, book := range books {
-			//Büyük-küçük harf duyarlılığını kaldırmak için hem kitapları hem de arama parametrelerini lowercase'e çeviriyoruz.
-			if strings.Contains(strings.ToLower(book), strings.ToLower(search_params)) {
-				result = append(result, book)
-			}
-		}
-
-		if len(result) == 0 {
-			fmt.Println("Aradığınız kriterde kitap bulunamamıştır.")
-		} else {
-			fmt.Println(result)
-		}
-
-	}
 }
